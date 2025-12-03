a785c13e78d400a7fc2ef1e8dce1761db50437b5
ruiter <38767@ma-web.nl>
2025-12-03 09:47:23 +0100

    item effects

--- Vertical slice - Darkest dungeon/Assets/Scripts/Item/Item.cs
+++ Vertical slice - Darkest dungeon/Assets/Scripts/Item/Item.cs
@@ -14,6 +14,16 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Clicked?.Invoke();
+            }
+
+        }
     }
 }
