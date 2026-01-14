af600c67b2595ef1c7e2c11af08f98dfb8aceb56
ruiter <38767@ma-web.nl>
2026-01-14 12:46:52 +0100

    item use wip

--- Vertical slice - Darkest dungeon/Assets/Scripts/Item/Buff.cs
+++ Vertical slice - Darkest dungeon/Assets/Scripts/Item/Buff.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Buff : MonoBehaviour
 {
     private float speed = 3f;
     private float timePassed = 0f;
+    private float negTime = 0.9f;
+
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         timePassed += Time.deltaTime;
+        negTime -= Time.deltaTime;
         transform.position += Vector3.up * Time.deltaTime * speed;
 
-        if (timePassed > 0.5f) 
+        spriteRenderer.color = new Color(1f, 1f, 1f, negTime);
+
+        if (timePassed > 1f) 
         { 
             Destroy(gameObject);
         }
--- Vertical slice - Darkest dungeon/Assets/Scripts/Item/ItemActivate.cs
+++ Vertical slice - Darkest dungeon/Assets/Scripts/Item/ItemActivate.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class ItemActivate : MonoBehaviour
 {
     private float timePassed = 0f;
+    private float negTime = 0.9f;
+
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        negTime -= Time.deltaTime;
         timePassed += Time.deltaTime;
+
+        
+
+        if (timePassed > 0.6f) 
+        {
+            spriteRenderer.color = new Color(1f, 1f, 1f, negTime);
+        }
+
         if (timePassed > 1.5f)
         {
             Destroy(gameObject);
